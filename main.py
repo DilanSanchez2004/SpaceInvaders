23eb6029ffa9b5638147fcc48b5f8c10d172804b
DilanSanchez2004 <sanchez.dilan@correounivalle.edu.co>
2024-06-01 17:07:41 -0500

    creacion de las clases
    + movimiento de personaje
    + configuración de pantalla
    + Fps configurados

--- main.py
+++ main.py
@@ -1,27 +1,49 @@
-# Example file showing a basic pygame "game loop"
+# Importación de la libreria "game loop"
 import pygame
 
-# pygame setup
+# Se define el lienzo y su tamaño
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+# icono del juego
+pygame_icon = pygame.image.load(
+    'C:\\Users\\user\\Desktop\\Space Invaders\\SpaceInvaders\\sprites\\icono_invader.jpg')
+pygame.display.set_icon(pygame_icon)
+pygame.display.set_caption('Space Invaders')  # titulo del juego
+screen = pygame.display.set_mode((640, 480))  # resolucion del juego
 clock = pygame.time.Clock()
 running = True
+dt = 0
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+
+player_pos = pygame.Vector2(
+    screen.get_width() / 2, screen.get_height() / 1.08)
+
+while running:  # ciclo infinito del juego
+
+    # Creacion de lienzo
+    for event in pygame.event.get():  # encuesta para eventos
+        if event.type == pygame.QUIT:  # evento pygame.QUIT significa que el usuario hizo clic en X para cerrar la ventana
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("Black")
+    screen.fill("black")
+
+    pygame.draw.circle(screen, "white", player_pos, 15)
+
+    keys = pygame.key.get_pressed()
+    # if keys[pygame.K_w]:
+    #     player_pos.y -= 300 * dt
+    # if keys[pygame.K_s]:
+    #     player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+    # llenar la pantalla con un color para borrar cualquier cosa del último fotograma
 
     # RENDER YOUR GAME HERE
 
-    # flip() the display to put your work on screen
+    # flip() la pantalla para poner tu trabajo en pantalla
     pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
+    dt = clock.tick(60) / 1000  # limite de FPS a 60
 
 pygame.quit()
---
+++ personaje.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+
+class personaje(ABC):
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.vida = 1
+        self.sprite = " "  # Estos es para el sprited de cada personaje
+
+    def atacar(self, objetivo):
+        pass
+
+    @abstractmethod
+    def moverse(self):
+        pass
+
+    @abstractmethod
+    def detruirse(self, vida):
+        pass
